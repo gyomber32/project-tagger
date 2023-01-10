0d297bd097c11ee64fe8e678e08fe7d7bc557a9c
ginger <gyomberattila@gmail.com>
2023-01-10 15:53:22 +0100

    navigate

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,10 +6,14 @@ import styles from "./Header.module.scss";
 import { projects } from "../../Data/projects";
 
 export const Header = () => {
+  const navigateToPage = () => {
+    // TODO
+  };
+
   return (
     <div className={styles.container}>
       <Logo onClick={() => {}} />
-      <SearchBar projects={projects} />
+      <SearchBar projects={projects} onClick={navigateToPage} />
       <div>Menu</div>
     </div>
   );
