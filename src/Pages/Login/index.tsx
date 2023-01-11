5f2be7a42acc10373e778ff9512b9ce5edf7be8e
ginger <gyomberattila@gmail.com>
2023-01-12 00:03:08 +0100

    login page form

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,28 +1,58 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { FormContainer } from "../../Layouts";
+import styles from "./Login.module.scss";
+import { Button } from "../../Components";
 
 interface ILoginFormInput {
   email: string;
   password: string;
 }
 
+//for registration
+const emailRegEx =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 export const LoginPage = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<ILoginFormInput>();
-  const onSubmit: SubmitHandler<ILoginFormInput> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<ILoginFormInput> = (data) => {
+    console.log(data);
+    navigate("/");
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <input {...register("email", { required: "Required" })} />
-      <p>{errors.email?.message}</p>
-      <label>Gender Selection</label>
-      <input {...register("password", { required: "Required" })} />
-      <p>{errors.password?.message}</p>
-      <input type="submit" />
-    </form>
+    <FormContainer>
+      <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.field}>
+          <input
+            placeholder="Email"
+            {...register("email", { required: "Required" })}
+          />
+          {errors.email?.message && (
+            <p className={styles.error}>{errors.email?.message}</p>
+          )}
+        </div>
+        <div className={styles.field}>
+          <input
+            placeholder="Password"
+            {...register("password", { required: "Required" })}
+          />
+          {errors.password?.message && (
+            <p className={styles.error}>{errors.password?.message}</p>
+          )}
+        </div>
+        <Button
+          className={styles.submit}
+          title="Submit"
+          type="submit"
+        />
+      </form>
+    </FormContainer>
   );
 };
